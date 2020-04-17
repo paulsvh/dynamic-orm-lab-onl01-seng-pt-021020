61a676917947242fad9db411f1f624456783a70a
Paul von Hagen <pvonhagen26@gmail.com>
2020-04-17 02:37:13 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,7 +31,27 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
-    self.class.table_name_for_insert
+    self.class.table_name
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = "select * from #{self.table_name} where name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+end
 
 
 
